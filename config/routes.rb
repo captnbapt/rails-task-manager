6d69c14e3f2a35cc3d5fee85ab2322a773a37d2f
Baptiste Decarra <baptiste.decarra@gmail.com>
2022-02-13 23:05:18 +0100

    passage sous rails v6

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+
+  # #See details about one restaurant
+  get 'tasks/new', to: 'tasks#new', as: 'task_new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get "tasks/:id/edit", to: "tasks#edit", as: 'task_edit'
+
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete "tasks/:id", to: "tasks#destroy"
+end
